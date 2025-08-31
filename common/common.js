76f4439b8018a802062f0b3f3649ad25716cb05c
Beat <>
2025-08-31 20:11:53 +0200

    dev

--- common/common.js
+++ common/common.js
@@ -10,9 +10,9 @@ function headBody(root, title, description) {
     document.writeln("<head>");
     document.writeln("<meta charset=\"UTF-8\">");
     document.writeln("<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">");
-    const css1 = root + "/css/simple.min.css";
+    const css1 = root + "/common/simple.min.css";
     document.writeln("<link rel=\"stylesheet\" href=\"" + css1 + "\">");
-    const css2 = root + "/css/styles.css";
+    const css2 = root + "/common/styles.css";
     document.writeln("<link rel=\"stylesheet\" href=\"" + css2 + "\">");
     const favicon = root + "/common/favicon.ico"
     document.writeln("<link rel=\"icon\" type=\"image/x-icon\" href=\"" + favicon + "\">");

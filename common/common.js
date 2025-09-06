fe8a5d35573840836bc9722353dc0be917192d06
Beat <>
2025-09-06 21:24:26 +0200

    dev

--- common/common.js
+++ common/common.js
@@ -100,6 +100,8 @@ function getFileForId(filename, id) {
 function getUrlResponse(url) {
     const request = new XMLHttpRequest();
     request.open("GET", url, true);
+    request.setRequestHeader('Accept', 'application/json');
+    request.setRequestHeader('Content-Type', 'application/json');
     request.onreadystatechange = function () {
         return request.responseText;
     }
